0444cef9109d012f2452c64708586441ff357b0c
Rafee <rafeehasan88@gmail.com>
2026-02-25 16:11:37 +0600

    architecture

---
+++ src/router/Router.jsx
@@ -0,0 +1,12 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "../components/layout/MainLayout";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+  },
+]);
+export default router;
